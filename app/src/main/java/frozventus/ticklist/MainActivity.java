43a2b5dfd09d09cae3d4191e010ee32dde8353ed
FrozVentus <FrozVentus@users.noreply.github.com>
2017-06-07 12:36:12 +0800

    Test commit

--- app/src/main/java/frozventus/ticklist/MainActivity.java
+++ app/src/main/java/frozventus/ticklist/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // comment line for testing change
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
