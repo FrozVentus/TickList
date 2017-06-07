3cc33cef5552290599cff0586cec0f0b0f5296b8
FrozVentus <FrozVentus@users.noreply.github.com>
2017-06-07 12:59:35 +0800

    Reverted test change

--- app/src/main/java/frozventus/ticklist/MainActivity.java
+++ app/src/main/java/frozventus/ticklist/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // comment line for testing changes
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
