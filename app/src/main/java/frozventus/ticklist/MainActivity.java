f676bc91c8223b206d4817e4185e58e3eb38ba2d
FrozVentus <FrozVentus@users.noreply.github.com>
2017-06-07 14:54:51 +0800

    Added expandable list view

--- app/src/main/java/frozventus/ticklist/MainActivity.java
+++ app/src/main/java/frozventus/ticklist/MainActivity.java
@@ -1,15 +1,27 @@
 package frozventus.ticklist;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ExpandedMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> activityList = null;
+    ArrayAdapter listAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+//Test Data
+        ListView listView = (ListView) findViewById(R.id.item_list);
+        activityList = new ArrayList<>();
+        activityList.add("First");
+        activityList.add("Second");
+        listAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
+        listView.setAdapter(listAdapter);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
